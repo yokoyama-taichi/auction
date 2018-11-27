a1ebab31e1aec52a98e13eba504d14ff0bc974f1
yokoyama-taichi <renraku451@gmail.com>
2018-11-27 16:51:29 +0900

    登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,18 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+  def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to @item
+  end
+
+  private 
+
+  def item_params
+    params.require(:item).permit(:name,:description,:price,:seller,:email,:image_url)
+  end
 end
